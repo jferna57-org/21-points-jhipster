e7a8cced053713ff017648465e9c0de5a177bb8c
jk <jferna57@gmail.com>
2016-12-06 17:58:59 +0100

    add weight entity

---
+++ src/main/java/net/juancarlosfernandez/jhipster/domain/Weight.java
@@ -0,0 +1,114 @@
+package net.juancarlosfernandez.jhipster.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+/**
+ * A Weight.
+ */
+@Entity
+@Table(name = "weight")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName = "weight")
+public class Weight implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(name = "date_time", nullable = false)
+    private ZonedDateTime dateTime;
+
+    @NotNull
+    @Column(name = "weight", nullable = false)
+    private Integer weight;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ZonedDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public Weight dateTime(ZonedDateTime dateTime) {
+        this.dateTime = dateTime;
+        return this;
+    }
+
+    public void setDateTime(ZonedDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public Weight weight(Integer weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Weight user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Weight weight = (Weight) o;
+        if (weight.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, weight.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Weight{" +
+            "id=" + id +
+            ", dateTime='" + dateTime + "'" +
+            ", weight='" + weight + "'" +
+            '}';
+    }
+}
