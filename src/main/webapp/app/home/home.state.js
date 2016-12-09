0b070c099f8c387f168584026cef11dfd3e99442
jk <jferna57@gmail.com>
2016-12-09 11:16:32 +0100

    Add linkt to modify the user preferences from home page

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -29,6 +29,39 @@
                 }]
             }
         })
+        .state('preferences.add', {
+            parent: 'home',
+            url: 'add/preferences',
+            data: {
+                authorities: ['ROLE_USER']
+            },
+            resolve: {
+                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    $translatePartialLoader.addPart('units');
+                    $translatePartialLoader.addPart('preferences');
+                    return $translate.refresh();
+                }]
+            },
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'app/entities/preferences/preferences-dialog.html',
+                    controller: 'PreferencesDialogController',
+                    controllerAs: 'vm',
+                    backdrop: 'static',
+                    size: 'lg',
+                    resolve: {
+                        entity: ['Preferences', function (Preferences) {
+                            return Preferences.user().$promise;
+                        }]
+                    }
+                }).result.then(function() {
+                    $state.go('home', null, { reload: true });
+                }, function() {
+                    $state.go('home');
+                });
+            }]
+        })
         .state('points.add', {
             parent: 'home',
             url: '/add/points',
