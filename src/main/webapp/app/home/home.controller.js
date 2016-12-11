5a9398a3c600841c6aa8ceec38c96bec1d9432cc
jk <jferna57@gmail.com>
2016-12-11 08:38:57 +0100

    add chart to the blood pressure for the last 30 days

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,41 +1,80 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('21PointsApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', 'Points', 'Preferences', '$state'];
+    HomeController.$inject = ['$scope', '$state', 'Chart', 'Principal', 'LoginService', 'Preferences', 'Points', 'BloodPressure', 'Weight'];
 
-    function HomeController ($scope, Principal, LoginService, Points, Preferences, $state) {
+    function HomeController($scope, $state, Chart, Principal, LoginService, Preferences, Points, BloodPressure, Weight) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
         });
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
-            });
+                if (vm.isAuthenticated()) {
+                    Preferences.user(function (preferences) {
+                        vm.preferences = preferences;
 
-            Preferences.user(function(data){
-                vm.preferences = data;
-            });
+                        Points.thisWeek(function (points) {
+                            vm.pointsThisWeek = points;
+                            vm.pointsPercentage = (points.points / vm.preferences.weeklyGoal) * 100;
+                        });
+                    });
 
-            Points.thisWeek(function(data){
-                vm.pointsThisWeek = data;
-                vm.pointsPercentage = (data.points / 21) * 100;
+                    BloodPressure.last30Days(function (bpReadings) {
+                        vm.bpReadings = bpReadings;
+                        if (bpReadings.readings.length) {
+                            vm.bpOptions = angular.copy(Chart.getBpChartConfig());
+                            vm.bpOptions.title.text = bpReadings.period;
+                            vm.bpOptions.chart.yAxis.axisLabel = "Blood Pressure";
+                            var systolics, diastolics, upperValues, lowerValues;
+                            systolics = [];
+                            diastolics = [];
+                            upperValues = [];
+                            lowerValues = [];
+                            bpReadings.readings.forEach(function (item) {
+                                systolics.push({
+                                    x: new Date(item.dateTime),
+                                    y: item.systolic
+                                });
+                                diastolics.push({
+                                    x: new Date(item.dateTime),
+                                    y: item.diastolic
+                                });
+                                upperValues.push(item.systolic);
+                                lowerValues.push(item.diastolic);
+                            });
+                            vm.bpData = [{
+                                values: systolics,
+                                key: 'Systolic',
+                                color: '#673ab7'
+                            }, {
+                                values: diastolics,
+                                key: 'Diastolic',
+                                color: '#03a9f4'
+                            }];
+                            // set y scale to be 10 more than max and min
+                            vm.bpOptions.chart.yDomain = [Math.min.apply(Math, lowerValues) - 10, Math.max.apply(Math, upperValues) + 10]
+                        }
+                    });
+                }
             });
         }
-        function register () {
+
+        function register() {
             $state.go('register');
         }
     }
